27c46d806b967f5514d5dc88cb0c4453ef2eae52
shopping <nhutphan.020101@gmail.com>
2023-07-26 08:33:39 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonline.g8y6gwl.mongodb.net',
+    DB_USER: 'nhutphan020101',
+    DB_PASS: 'thanhnhut',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'nhutphan.020101@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'obuxbleasjoxkrzz',
+    JWT_SECRET: '123456',
+    JWT_EXPIRES: '6000000', // in milliseconds
+  };
+  module.exports = MyConstants;
